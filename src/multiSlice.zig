259f6d5101898c4617915966db2e52a68bc2af50
Traxar <trxr2a@gmail.com>
2025-07-13 08:52:42 +0200

    add de-&init

--- src/multiSlice.zig
+++ src/multiSlice.zig
@@ -1,6 +1,23 @@
 const std = @import("std");
 const expect = std.testing.expect;
 const assert = std.debug.assert;
+const Allocator = std.mem.Allocator;
+
+inline fn len(T: type) usize {
+    return switch (@typeInfo(T)) {
+        .array => |a| a.len,
+        .vector => |v| v.len,
+        else => unreachable,
+    };
+}
+
+fn child(T: type) type {
+    return switch (@typeInfo(T)) {
+        .array => |a| a.child,
+        .vector => |v| v.child,
+        else => unreachable,
+    };
+}
 
 /// recurrsive MultiPointer without functions
 fn MultiPointer(Element: type) type {
@@ -24,8 +41,7 @@ fn MultiPointer(Element: type) type {
                 .is_tuple = false,
             } });
         },
-        .array => |a| [a.len]MultiPointer(a.child),
-        .vector => |v| [v.len]MultiPointer(v.child),
+        .array, .vector => [len(Element)]MultiPointer(child(Element)),
         else => [*]Element,
     };
 }
@@ -49,8 +65,7 @@ pub fn MultiSlice(comptime Element: type) type {
 
         fn sub(slice: Slice, comptime field: anytype) switch (@typeInfo(Element)) {
             .@"struct" => MultiSlice(field.type),
-            .array => |a| MultiSlice(a.child),
-            .vector => |v| MultiSlice(v.child),
+            .array, .vector => MultiSlice(child(Element)),
             else => unreachable,
         } {
             return switch (@typeInfo(Element)) {
@@ -74,20 +89,71 @@ pub fn MultiSlice(comptime Element: type) type {
                         _ = slice.sub(field);
                     }
                 },
-                .array => |a| {
-                    inline for (0..a.len) |i| {
+                .array, .vector => {
+                    inline for (0..len(Element)) |i| {
                         _ = slice.sub(i);
                     }
                 },
-                .vector => |v| {
-                    inline for (0..v.len) |i| {
-                        _ = slice.sub(i);
-                    }
-                },
-                else => { //end case
+                else => {
                     assert(Pointer == [*]Element);
                 },
             }
+        }
+
+        pub fn deinit(slice: Slice, arena: Allocator) void {
+            switch (@typeInfo(Element)) {
+                .@"struct" => |s| {
+                    inline for (s.fields) |field| {
+                        slice.sub(field).deinit(arena);
+                    }
+                },
+                .array, .vector => {
+                    inline for (0..len(Element)) |i| {
+                        slice.sub(i).deinit(arena);
+                    }
+                },
+                else => {
+                    arena.free(slice.ptr[0..slice.len]);
+                },
+            }
+        }
+
+        pub fn init(n: usize, arena: Allocator) !Slice {
+            var slice: Slice = undefined;
+            slice.len = n;
+            switch (@typeInfo(Element)) {
+                .@"struct" => |s| {
+                    inline for (s.fields, 0..) |field, i| {
+                        @field(slice.ptr, field.name) = (MultiSlice(field.type).init(n, arena) catch |err| {
+                            inline for (0..i) |j| {
+                                slice.sub(s.fields[j]).deinit(arena);
+                            }
+                            return err;
+                        }).ptr;
+                    }
+                },
+                .array, .vector => {
+                    inline for (0..len(Element)) |i| {
+                        slice.ptr[i] = (MultiSlice(child(Element)).init(n, arena) catch |err| {
+                            inline for (0..i) |j| {
+                                slice.sub(j).deinit(arena);
+                            }
+                            return err;
+                        }).ptr;
+                    }
+                },
+                else => {
+                    slice.ptr = (try arena.alloc(Element, n)).ptr;
+                },
+            }
+            return slice;
+        }
+
+        test init {
+            const ally = std.testing.allocator;
+            const a = try Slice.init(10, ally);
+            defer a.deinit(ally);
+            //? do stuff with a
         }
     };
 }
